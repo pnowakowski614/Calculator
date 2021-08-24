9dfcc0438d7131bb779bf2a751854bcae51c0d55
pnowakowski614 <p.nowakowski416@gmail.com>
2021-08-24 20:28:40 +0200

    Basic functions + initial calculator layout

---
+++ script.js
@@ -0,0 +1,32 @@
+function add(a,b) {
+    return a + b;
+}
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function subtract(a,b) {
+    return a - b;
+}
+
+function divide(a,b) {
+    if(b === 0) {
+        return "You can't divide by zero!";
+    }
+    return a / b;
+}
+
+function operate(a, operator, b) {
+    switch(operator) {
+        case '+': 
+            return add(a,b);
+        case '-': 
+            return subtract(a,b);
+        case '*': 
+            return multiply(a,b);
+        case '/': 
+            return divide(a,b);
+        default: break;
+    }
+}
