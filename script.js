ccb582bdbe6c638651933fe69391ec5bb01edd0e
pnowakowski614 <p.nowakowski416@gmail.com>
2021-08-25 23:19:47 +0200

    Added basic operations and fixed bugs

--- script.js
+++ script.js
@@ -2,38 +2,62 @@ let numbers = document.querySelectorAll(".numbers");
 let operators = document.querySelectorAll(".operators");
 let display = document.getElementById("display");
 let clear = document.getElementById("clear");
+let equals = document.getElementById("equals");
 let displayValue;
+let firstValue;
+let secondValue;
+let result = undefined;
+let newNumber = false;
 display.textContent = "0";
 const limit = 11;
 
-clear.addEventListener('click', function() {
-    display.textContent = "0";
-    displayUpdate();
-})
 
-function displayUpdate() {
+function valueUpdate() {
     displayValue = Number(display.textContent);
+}
+
+function displayUpdate(number) {
+    if(newNumber === false) {
+        if (display.textContent.length !== limit) {
+            if (display.textContent === "0") display.textContent = number;
+            else display.textContent += number;                
+        }  
+    }
+    else {
+        newNumber = false;
+        display.textContent = number;
+    }
 }
 
 for (let i = 0; i < numbers.length; ++i) {
     numbers[i].addEventListener('click', function() {
-        if (display.textContent.length !== limit) {
-            if (display.textContent === "0") {
-                display.textContent = numbers[i].textContent;
-            }
-            else {
-                display.textContent += numbers[i].textContent;
-            }
-        }
-    displayUpdate();
+    displayUpdate(numbers[i].textContent)
+    valueUpdate();
     }) 
 }
+
 
 for (let j = 0; j < operators.length; ++j) {
     operators[j].addEventListener('click', function() {
         chosenOperator = operators[j].id;
+        if (result === undefined) firstValue = displayValue;
+        newNumber = true;
     })
 }
+
+clear.addEventListener('click', function() {
+    display.textContent = "0";
+    firstValue = 0;
+    secondValue = 0;
+    result = undefined;
+    valueUpdate();
+})
+
+equals.addEventListener('click', function() {
+    secondValue = displayValue;
+    result = operate(firstValue, chosenOperator, secondValue);
+    display.textContent = firstValue = result;
+})
 
 function add(a,b) {
     return a + b;
